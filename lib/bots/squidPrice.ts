b55036f478a558e5f777df8d6c1694a153a85cca
Matthew Bunday <mkbunday@gmail.com>
2021-12-21 19:35:49 -0500

    Add format function

---
+++ lib/bots/squidPrice.ts
@@ -0,0 +1,44 @@
+import Bot from "./bot";
+import { formatDollars, formatEth } from "./utils/format";
+
+class SquidPrice extends Bot {
+  lastPrice?: number;
+
+  constructor() {
+    super(process.env.SQUID_PRICE_USD);
+    this.lastPrice = undefined;
+  }
+
+  getMomentum(price: number) {
+    if (!this.lastPrice) {
+      return "→";
+    }
+    if (this.lastPrice == price) {
+      return "→";
+    }
+
+    const updown = this.lastPrice < price ? "↗" : "↘";
+    return `${updown}${formatDollars(Math.abs(this.lastPrice - price))}`;
+  }
+
+  async update({
+    squidEthPrice,
+    ethUsdPrice,
+  }: {
+    squidEthPrice: number;
+    ethUsdPrice: number;
+    squidSupply: number;
+  }) {
+    const price = squidEthPrice;
+    const ethPrice = ethUsdPrice;
+    const usdPrice = price * ethPrice;
+    const momentum = this.getMomentum(usdPrice);
+
+    this.setNickname(`\$${formatDollars(usdPrice)} ${momentum}`);
+    this.setStatus("Price in USD");
+
+    this.lastPrice = usdPrice;
+  }
+}
+
+export default SquidPrice;
--- lib/bots/utils/format.ts
+++ lib/bots/utils/format.ts
@@ -1,17 +1,35 @@
-export const formatEth = (number: number): string =>
+export type Formatter = (number: number) => string;
+
+export const formatEth: Formatter = (number) =>
   number.toLocaleString(undefined, {
     maximumFractionDigits: 1,
     minimumFractionDigits: 1,
   });
 
-export const formatDollars = (number: number): string =>
+export const formatDollars: Formatter = (number) =>
   number.toLocaleString(undefined, {
     maximumFractionDigits: 0,
     minimumFractionDigits: 0,
   });
 
-export const formatCents = (number: number): string =>
+export const formatCents: Formatter = (number) =>
   number.toLocaleString(undefined, {
     maximumFractionDigits: 2,
     minimumFractionDigits: 2,
   });
+
+export const formatMomentum = (
+  formatter: Formatter,
+  number?: number,
+  lastNumber?: number
+): string => {
+  if (!number || !lastNumber) {
+    return "→";
+  }
+  if (number == lastNumber) {
+    return "→";
+  }
+
+  const updown = lastNumber < number ? "↗" : "↘";
+  return `${updown}${formatter(Math.abs(lastNumber - number))}`;
+};
