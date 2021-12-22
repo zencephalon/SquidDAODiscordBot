d82269836ce425ea6ea84dc1c0cc3bc8ccadf9a7
Matthew Bunday <mkbunday@gmail.com>
2021-12-21 19:54:17 -0500

    Setup for new structure

--- lib/bots/bot.ts
+++ lib/bots/bot.ts
@@ -1,20 +1,50 @@
 import { Client, Intents } from "discord.js";
 
+export interface BotInputs {
+  squidEthPrice: number;
+  ethUsdPrice: number;
+  squidSupply: number;
+}
+
+interface Display {
+  label: string;
+  getDisplay: (input?: BotInputs) => string;
+}
+
 class Bot {
   client: Client;
   token?: string;
+  displays?: Display[];
+  inputs?: BotInputs;
+  displayIndex = 0;
 
-  constructor(token?: string, label?: string) {
+  constructor(token?: string, displays?: Display[]) {
     this.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
     this.token = token;
     this.client.on("debug", console.log);
+    this.displays = displays;
   }
 
   async init() {
     return this.client.login(this.token);
   }
 
-  async update(data: any) {}
+  async update(inputs: BotInputs) {
+    this.inputs = inputs;
+  }
+
+  async refreshDisplay() {
+    if (!this.displays) {
+      return;
+    }
+    const displayNum = this.displays.length;
+    const display = this.displays[this.displayIndex];
+
+    this.setNickname(display.getDisplay(this.inputs));
+    this.setStatus(display.label);
+
+    this.displayIndex = (this.displayIndex + 1) % displayNum;
+  }
 
   async setNickname(name: string) {
     const guilds = await this.client.guilds.cache;
